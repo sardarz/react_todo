820d96b7e210ef86f2c5b6c4e562831b6c2762c8
sardarz <muhkween@gmail.com>
2021-05-10 11:43:52 +0300

    add check icon

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -3,16 +3,24 @@ import './TodoAdd.scss';
 
 const TodoAdd = () => {
   const [value, setValue] = useState('');
+  const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
-    console.log(value);
+    // console.log(value);
   });
   return (
     <div className="todoadd">
+      <div
+        className={`circle ${clicked ? 'clicked' : ''}`}
+        onClick={() => setClicked(!clicked)}
+      >
+        <i className="fas fa-check"></i>
+      </div>
       <input
         className="todoadd__input"
         value={value}
         onChange={(evt) => setValue(evt.target.value)}
+        placeholder="Add your todo entry"
       />
     </div>
   );
