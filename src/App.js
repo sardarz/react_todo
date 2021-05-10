fe5909029cb545a99ba5dca92080d7dcf97d96db
sardarz <muhkween@gmail.com>
2021-05-10 13:17:01 +0300

    start from scratch

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
-import TodoAdd from './components/TodoAdd';
-import TodoList from './components/TodoList'
 import bgImg from './images/bg-desktop-dark.jpg';
 import './App.scss';
 
 const App = () => {
   const [theme, setTheme] = useState(true);
-  const [list, setList] = useState([]);
+  // const [list, setList] = useState([]);
 
   useEffect(() => {
-    // console.log(theme);
-    console.log(list);
-  });
+    console.log(theme);
+  })
 
   return (
     <>
       <img className="bgImg" src={bgImg} alt="" />
       <div className="container">
         <Header onClick={setTheme} theme={theme} />
-        <TodoAdd list={list} setList={setList} />
-        <TodoList list={list} />
-        {list}
       </div>
     </>
   );
